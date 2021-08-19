a1efb2c53fe80060a55b7b41162151ea30b7adc6
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-08-19 18:00:03 +0200

    add figma embeds example

---
+++ figma-example/envs/my-react/my-react.aspect.ts
@@ -0,0 +1,5 @@
+import { Aspect } from '@teambit/harmony';
+
+export const MyReactAspect = Aspect.create({
+  id: 'learn-bit-react.figma-example/envs/my-react'
+});
---
+++ figma-example/envs/my-react/my-react.preview.runtime.ts
@@ -0,0 +1,20 @@
+import { PreviewRuntime } from '@teambit/preview';
+import { ReactAspect, ReactPreview } from '@teambit/react';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
+import { MyReactAspect } from './my-react.aspect';
+
+export class MyReactPreviewMain {
+  static runtime = PreviewRuntime;
+
+  static dependencies = [ReactAspect];
+
+  static async provider([react]: [ReactPreview]) {
+    const myReactPreviewMain = new MyReactPreviewMain();
+    // register a new provider to wrap all compositions using this environment with a custom theme.
+    react.registerProvider([ThemeCompositions]);
+
+    return myReactPreviewMain;
+  }
+}
+
+MyReactAspect.addRuntime(MyReactPreviewMain);
